6d61a6c022a87dc388c2cdf6ea685aa3e6059e4f
Andes Lam <lamandes@outlook.com>
2019-05-19 08:58:48 +0100

    Temporary solution for local JSON file to stimulate JSON file parse.

--- Andes_EPC/Controllers/HomeController.cs
+++ Andes_EPC/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,6 +22,14 @@ namespace Andes_EPC.Controllers
             ViewBag.Message = "Runnymede energy rating page.";
             //var json = "epc_testJSON2.json";
             return View();
+        }
+
+        public object Get()
+        {
+            //string allText = System.IO.File.ReadAllText(@Server.MapPath("~/App_Data/epc_testJSON2.json"));
+            string allText = System.IO.File.ReadAllText(@Server.MapPath("~/App_Data/test.json"));
+            object jsonObject = JsonConvert.DeserializeObject(allText);
+            return jsonObject;
         }
 
         public ActionResult About()
